6cefd04ddf9836623a4d72683651d294e1afea6e
Jucemeire Lopes <jucemeirelopes@gmail.com>
2023-02-07 23:31:50 -0300

    aula 11.7

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -3,8 +3,10 @@ package com.algaworks.algafood.core.jackson;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.mixin.CidadeMixin;
+import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
 import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -16,8 +18,12 @@ public class JacksonMixinModule extends  SimpleModule{
 	public JacksonMixinModule () {
 		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 		setMixInAnnotation(Cidade.class, CidadeMixin.class);
+		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+
 
 		
 	}
 
 }
+
+
